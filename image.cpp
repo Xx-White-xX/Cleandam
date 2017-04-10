da21337c8860a77995e0b07dd72ff80a67b48966
TSITCL\s162037 <crown.got9610@gmail.com>
2017-04-10 16:28:08 +0900

    cpp,hファイル

---
+++ image.cpp
@@ -0,0 +1,19 @@
+#include "image.h"
+
+
+
+image::image(Vector2 pos)
+{
+	grp_h = 480;
+	grp_w = 640;
+	grp_x = 0;
+	grp_y = 0;
+
+	handle = new Texture(L"Resources\\images\\title.png");
+}
+
+
+image::~image()
+{
+
+}
